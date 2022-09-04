85d435ac95701ca9a518b48e6e9d56d62b8da620
Dalibor Pavlov <pavlov.dalibor@hotmail.com>
2022-09-04 14:52:02 +0200

    Add autosave to localStorage

--- focus/js/init.js
+++ focus/js/init.js
@@ -1,11 +1,32 @@
-let tools = ace.require("ace/ext/language_tools");
 let editor = ace.edit("editor");
 editor.setTheme("ace/theme/tomorrow_night_eighties");
 editor.session.setMode("ace/mode/lua");
 editor.setOptions({
-    enableSnippets: false,
-    enableLiveAutocompletion: true
+
 })
 editor.container.style.lineHeight = 1.5
-editor.font
 editor.renderer.updateFontSize()
+
+let detector = document.querySelector('.ace_text-input')
+
+detector.addEventListener('input', e => {
+    save()
+})
+
+onload = function () {
+    if (this.localStorage.getItem('code')) {
+        editor.setValue(localStorage.getItem('code'))
+    }
+}
+
+onkeydown = function (e) {
+    if (e.ctrlKey && e.keyCode == 'S'.charCodeAt(0)) {
+        e.preventDefault();
+        save()
+    }
+}
+
+save = function () {
+    const content = editor.getValue()
+    localStorage.setItem('code', content)
+}
